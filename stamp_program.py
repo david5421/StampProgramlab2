5f5d8e55e712222473e2ffb3388c92a57ded561e
David <98560756+david5421@users.noreply.github.com>
2022-05-16 17:47:46 -0700

    Add files via upload

---
+++ stamp_program.py
@@ -0,0 +1,26 @@
+"""
+start
+get the numbers of sheets
+sheets / 5
+round answer to next number
+output the results to the user
+end
+"""
+import math
+
+#input sheet
+def calucalte(sheet):
+    #step 1
+    answer = sheet /5
+    #step 2
+    rounded = math.ceil(answer)
+    print("sheet is : ", sheet)
+    print("The answer is: ", answer)
+    print("rounded is: ", rounded)
+    print("==================================")
+    # output: number of stamps needed
+    return rounded
+
+output = calculate(16)
+
+print("The number of stamps needed is: ", output)
